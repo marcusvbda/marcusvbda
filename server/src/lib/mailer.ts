8b666c5ca3ed6b810658e740681e1604b9590b56
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2025-02-12 18:50:53 -0300

    debug

--- server/src/lib/mailer.ts
+++ server/src/lib/mailer.ts
@@ -3,27 +3,33 @@ import axios from 'axios';
 export const sendEmail = async (params: any = {}): Promise<void> => {
 	const API_URL = 'https://api.brevo.com/v3/smtp/email';
 
-	axios
-		.post(
-			API_URL,
-			{
-				...params,
-				sender: {
-					name: process.env.SENDINBLUE_SENDER_NAME,
-					email: process.env.SENDINBLUE_SENDER_EMAIL,
+	return new Promise((resolve: any) => {
+		axios
+			.post(
+				API_URL,
+				{
+					...params,
+					sender: {
+						name: process.env.SENDINBLUE_SENDER_NAME,
+						email: process.env.SENDINBLUE_SENDER_EMAIL,
+					},
 				},
-			},
-			{
-				headers: {
-					'api-key': process.env.SENDINBLUE_API_KEY || '',
-					'Content-Type': 'application/json',
-				},
-			}
-		)
-		.catch((error) => {
-			console.error(
-				'Erro ao enviar e-mail:',
-				error.response?.data || error.message
-			);
-		});
+				{
+					headers: {
+						'api-key': process.env.SENDINBLUE_API_KEY || '',
+						'Content-Type': 'application/json',
+					},
+				}
+			)
+			.then(() => {
+				resolve('enviado com sucesso');
+			})
+			.catch((error) => {
+				resolve(
+					'Erro ao enviar e-mail:' + error?.response?.data ||
+						error?.message ||
+						''
+				);
+			});
+	});
 };
