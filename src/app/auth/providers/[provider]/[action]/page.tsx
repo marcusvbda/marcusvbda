a9b7a02f8b3436894cfbfa5255c6455e96d04844
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2025-02-16 08:03:25 -0300

    volta logica providers

--- src/app/auth/providers/[provider]/[action]/page.tsx
+++ src/app/auth/providers/[provider]/[action]/page.tsx
@@ -1,86 +1,86 @@
 'use client';
-// import { enabledOAuthProviders } from '@/constants/providers';
-// import { authRoutes } from '@/constants/routes';
+import { enabledOAuthProviders } from '@/constants/providers';
+import { authRoutes } from '@/constants/routes';
 import { Loader2 } from 'lucide-react';
-// import { useRouter } from 'next/navigation';
-// import { useEffect, useState } from 'react';
-// import {
-// 	callbackGithub,
-// 	callbackGoogle,
-// 	redirectGithub,
-// 	redirectGoogle,
-// } from './actions';
-// import { useFetch } from '@/hooks/use-fetch';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import {
+	callbackGithub,
+	callbackGoogle,
+	redirectGithub,
+	redirectGoogle,
+} from './actions';
+import { useFetch } from '@/hooks/use-fetch';
 
 export default function OAuth({ params, searchParams }: any) {
-	// const { fetcher } = useFetch();
-	// const { receiveProviderUser } = authRoutes;
-	// const { provider, action } = params;
-	// const { code } = searchParams;
-	// const [error, setError] = useState('');
-	// const router = useRouter();
+	const { fetcher } = useFetch();
+	const { receiveProviderUser } = authRoutes;
+	const { provider, action } = params;
+	const { code } = searchParams;
+	const [error, setError] = useState('');
+	const router = useRouter();
 
-	// useEffect(() => {
-	// 	const origin = window.location.origin;
-	// 	const path = window.location.pathname;
-	// 	const originalUrl = `${origin}${path}`;
+	useEffect(() => {
+		const origin = window.location.origin;
+		const path = window.location.pathname;
+		const originalUrl = `${origin}${path}`;
 
-	// 	const actions: any = {
-	// 		callback: async (receivedProvider: any) => {
-	// 			const handlers: any = {
-	// 				github: callbackGithub,
-	// 				google: callbackGoogle,
-	// 			};
-	// 			if (!code) return setError('no-redirect-code');
-	// 			const action = handlers[receivedProvider];
-	// 			if (!action) return setError('no-redirect-action');
-	// 			const userInfo = await action(originalUrl, code);
-	// 			if (userInfo === 'error') return setError('no-redirect-userinfo');
+		const actions: any = {
+			callback: async (receivedProvider: any) => {
+				const handlers: any = {
+					github: callbackGithub,
+					google: callbackGoogle,
+				};
+				if (!code) return setError('no-redirect-code');
+				const action = handlers[receivedProvider];
+				if (!action) return setError('no-redirect-action');
+				const userInfo = await action(originalUrl, code);
+				if (userInfo === 'error') return setError('no-redirect-userinfo');
 
-	// 			fetcher(
-	// 				{
-	// 					route: receiveProviderUser,
-	// 					method: 'POST',
-	// 					body: userInfo,
-	// 				},
-	// 				{
-	// 					onSuccess: (user: any) => {
-	// 						console.log('logar com', user);
-	// 						router.push('/admin');
-	// 					},
-	// 				},
-	// 			);
-	// 		},
-	// 		redirect: async (receivedProvider: string) => {
-	// 			const handlers: any = {
-	// 				google: redirectGoogle,
-	// 				github: redirectGithub,
-	// 			};
-	// 			const action = handlers[receivedProvider];
-	// 			if (!action) return setError('no-callback-action');
-	// 			const redirectUrl = await action(originalUrl);
-	// 			router.push(redirectUrl);
-	// 		},
-	// 	};
+				fetcher(
+					{
+						route: receiveProviderUser,
+						method: 'POST',
+						body: userInfo,
+					},
+					{
+						onSuccess: (user: any) => {
+							console.log('logar com', user);
+							router.push('/admin');
+						},
+					},
+				);
+			},
+			redirect: async (receivedProvider: string) => {
+				const handlers: any = {
+					google: redirectGoogle,
+					github: redirectGithub,
+				};
+				const action = handlers[receivedProvider];
+				if (!action) return setError('no-callback-action');
+				const redirectUrl = await action(originalUrl);
+				router.push(redirectUrl);
+			},
+		};
 
-	// 	const handleAction = async (
-	// 		receivedProvider: string,
-	// 		receivedAction: string,
-	// 	) => {
-	// 		if (!enabledOAuthProviders.includes(receivedProvider)) {
-	// 			return setError('provider-disabled');
-	// 		}
-	// 		const action = actions[receivedAction];
-	// 		if (!action) return setError('no-handle-action');
-	// 		await action(receivedProvider);
-	// 	};
+		const handleAction = async (
+			receivedProvider: string,
+			receivedAction: string,
+		) => {
+			if (!enabledOAuthProviders.includes(receivedProvider)) {
+				return setError('provider-disabled');
+			}
+			const action = actions[receivedAction];
+			if (!action) return setError('no-handle-action');
+			await action(receivedProvider);
+		};
 
-	// 	handleAction(provider, action);
-	// });
+		handleAction(provider, action);
+	});
 
-	// useEffect(() => {
-	// 	console.log(error);
-	// }, [error]);
+	useEffect(() => {
+		console.log(error);
+	}, [error]);
 
 	return (
 		<div className="w-full flex items-center justify-center py-10">
