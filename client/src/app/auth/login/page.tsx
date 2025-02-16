bc18dcbab0f1be1cd0b68b88800b33bd06e3f2c2
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2025-02-16 06:49:53 -0300

    TESTE 3

--- client/src/app/auth/login/page.tsx
+++ client/src/app/auth/login/page.tsx
@@ -11,70 +11,84 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import Link from 'next/link';
 import ProvidersLogin from '@/components/providers-login';
+import { GalleryVerticalEnd } from 'lucide-react';
 
 export default function LoginPage() {
 	return (
-		<div className={cn('flex flex-col gap-6')}>
-			<Card>
-				<CardHeader className="text-center">
-					<CardTitle className="text-xl">Welcome back</CardTitle>
-					<CardDescription>
-						Login with your Apple or Google account
-					</CardDescription>
-				</CardHeader>
-				<CardContent>
-					<form>
-						<div className="grid gap-6">
-							<ProvidersLogin />
-							<div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
-								<span className="relative z-10 bg-background px-2 text-muted-foreground">
-									Or continue with
-								</span>
-							</div>
+		<>
+			<Link
+				href="/"
+				prefetch={false}
+				className="flex items-center gap-2 self-center font-medium"
+			>
+				<div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+					<GalleryVerticalEnd className="size-4" />
+				</div>
+				Acme Inc.
+			</Link>
+
+			<div className={cn('flex flex-col gap-6')}>
+				<Card>
+					<CardHeader className="text-center">
+						<CardTitle className="text-xl">Welcome back</CardTitle>
+						<CardDescription>
+							Login with your Apple or Google account
+						</CardDescription>
+					</CardHeader>
+					<CardContent>
+						<form>
 							<div className="grid gap-6">
-								<div className="grid gap-2">
-									<Label htmlFor="email">Email</Label>
-									<Input
-										id="email"
-										type="email"
-										placeholder="m@example.com"
-										required
-									/>
+								<ProvidersLogin />
+								<div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+									<span className="relative z-10 bg-background px-2 text-muted-foreground">
+										Or continue with
+									</span>
 								</div>
-								<div className="grid gap-2">
-									<div className="flex items-center">
-										<Label htmlFor="password">Password</Label>
-										<a
-											href="#"
-											className="ml-auto text-sm underline-offset-4 hover:underline"
-										>
-											Forgot your password?
-										</a>
+								<div className="grid gap-6">
+									<div className="grid gap-2">
+										<Label htmlFor="email">Email</Label>
+										<Input
+											id="email"
+											type="email"
+											placeholder="m@example.com"
+											required
+										/>
 									</div>
-									<Input id="password" type="password" required />
+									<div className="grid gap-2">
+										<div className="flex items-center">
+											<Label htmlFor="password">Password</Label>
+											<a
+												href="#"
+												className="ml-auto text-sm underline-offset-4 hover:underline"
+											>
+												Forgot your password?
+											</a>
+										</div>
+										<Input id="password" type="password" required />
+									</div>
+									<Link href="/admin" prefetch={false}>
+										<Button className="w-full">Login</Button>
+									</Link>
 								</div>
-								<Link href="/admin" prefetch={false}>
-									<Button className="w-full">Login</Button>
-								</Link>
+								<div className="text-center text-sm">
+									{'Don`t'} have an account?{' '}
+									<Link
+										prefetch={false}
+										href="/auth/register"
+										className="underline underline-offset-4"
+									>
+										Register
+									</Link>
+								</div>
 							</div>
-							<div className="text-center text-sm">
-								{'Don`t'} have an account?{' '}
-								<Link
-									prefetch={false}
-									href="/auth/register"
-									className="underline underline-offset-4"
-								>
-									Register
-								</Link>
-							</div>
-						</div>
-					</form>
-				</CardContent>
-			</Card>
-			<div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary  ">
-				By clicking continue, you agree to our <a href="#">Terms of Service</a>{' '}
-				and <a href="#">Privacy Policy</a>.
+						</form>
+					</CardContent>
+				</Card>
+				<div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary  ">
+					By clicking continue, you agree to our{' '}
+					<a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.
+				</div>
 			</div>
-		</div>
+		</>
 	);
 }
--- client/src/app/auth/register/page.tsx
+++ client/src/app/auth/register/page.tsx
@@ -7,6 +7,7 @@ import { useTranslations } from 'next-intl';
 import FormMainStep from './form-main-step';
 import FormCodeCheckStep from './form-code-check-step';
 import FormRegisterStep from './form-register-step';
+import { GalleryVerticalEnd } from 'lucide-react';
 
 export default function RegisterPage() {
 	const t = useTranslations('RegisterPage');
@@ -15,35 +16,47 @@ export default function RegisterPage() {
 	const [step, setStep] = useState('main');
 
 	return (
-		<div className={cn('flex flex-col gap-6')}>
-			<Card>
-				{step === 'main' && (
-					<FormMainStep
-						onSubmit={(x: any) => {
-							setEmail(x.email);
-							setStep('code-check');
-						}}
-					/>
-				)}
-				{step === 'code-check' && (
-					<FormCodeCheckStep
-						email={email}
-						onSubmit={(code: string) => {
-							setStep('register-form');
-							setCodeResult(code);
-						}}
-					/>
-				)}
-				{step === 'register-form' && (
-					<FormRegisterStep email={email} codeResult={codeResult} />
-				)}
-			</Card>
-			<div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary  ">
-				{t('already_have_account')}{' '}
-				<Link prefetch={false} href="/auth/login">
-					{t('login')}.
-				</Link>
+		<>
+			<Link
+				href="/"
+				prefetch={false}
+				className="flex items-center gap-2 self-center font-medium"
+			>
+				<div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+					<GalleryVerticalEnd className="size-4" />
+				</div>
+				Acme Inc.
+			</Link>
+			<div className={cn('flex flex-col gap-6')}>
+				<Card>
+					{step === 'main' && (
+						<FormMainStep
+							onSubmit={(x: any) => {
+								setEmail(x.email);
+								setStep('code-check');
+							}}
+						/>
+					)}
+					{step === 'code-check' && (
+						<FormCodeCheckStep
+							email={email}
+							onSubmit={(code: string) => {
+								setStep('register-form');
+								setCodeResult(code);
+							}}
+						/>
+					)}
+					{step === 'register-form' && (
+						<FormRegisterStep email={email} codeResult={codeResult} />
+					)}
+				</Card>
+				<div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary  ">
+					{t('already_have_account')}{' '}
+					<Link prefetch={false} href="/auth/login">
+						{t('login')}.
+					</Link>
+				</div>
 			</div>
-		</div>
+		</>
 	);
 }
