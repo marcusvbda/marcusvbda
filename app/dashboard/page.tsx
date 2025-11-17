fa3a42400516cfc864e66d99c5d8775bb772562d
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2025-11-16 21:14:56 -0300

    resolve

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,7 @@
-import { SettingsDialog } from "@/components/settings-dialog"
-
 export default function Page() {
-  return (
-    <div className="flex h-svh items-center justify-center">
-      <SettingsDialog />
-    </div>
-  )
+	return (
+		<div className="flex h-svh items-center justify-center">
+			<h1>Dashboard</h1>
+		</div>
+	);
 }
