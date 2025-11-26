142afe3b213c422eb21e87e42aa62ecfd228ecaf
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2025-11-26 13:47:02 -0300

    resolve stripe

--- server/stripe.ts
+++ server/stripe.ts
@@ -17,7 +17,7 @@ export const createCheckoutSession = async (
 		const session = await stripe.checkout.sessions.create({
 			payment_method_types: ['card'],
 			mode: 'payment',
-			success_url: `${process.env.STRIPE_CALLBACK_URL}/payment-callback/{CHECKOUT_SESSION_ID}`,
+			success_url: `${process.env.STRIPE_CALLBACK_URL}/{CHECKOUT_SESSION_ID}`,
 			line_items: [
 				{
 					price_data: {
