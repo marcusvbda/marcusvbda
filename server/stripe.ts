a6bbeffe1088810bd1c73ceaae2aab77b7493a67
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2025-12-16 20:32:36 -0300

    corrige stripe

--- server/stripe.ts
+++ server/stripe.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe';
 
 const getStripeInstance = () => {
 	return new Stripe(process.env.STRIPE_SECRET_KEY!, {
-		apiVersion: '2025-11-17.clover',
+		apiVersion: '2025-12-15.clover',
 		typescript: true,
 	});
 };
@@ -39,8 +39,8 @@ export const createCheckoutSession = async (
 			},
 			metadata: message
 				? {
-						donation_message: message,
-				  }
+					donation_message: message,
+				}
 				: undefined,
 		});
 
